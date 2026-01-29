b9fbe92de3dcd7b81005e27275fcde7953f29640
lpandath <lthaci@tugraz.at>
2026-01-29 12:16:16 +0100

    Add custom curation request and record permission policies

--- invenio_override/permissions.py
+++ invenio_override/permissions.py
@@ -15,6 +15,18 @@ from invenio_records_permissions.generators import (
     IfConfig,
     SystemProcess,
 )
+from invenio_rdm_records.requests import CommunitySubmission
+from invenio_rdm_records.services.permissions import RDMRequestsPermissionPolicy
+from invenio_requests.services.generators import Creator, Receiver
+from invenio_curations.requests.curation import CurationRequest
+from invenio_curations.services.generators import (
+    IfCurationRequestAccepted,
+    IfRequestTypes,
+    TopicPermission,
+)
+from invenio_curations.services.permissions import (
+    CurationRDMRecordPermissionPolicy as BaseCurationRDMRecordPermissionPolicy,
+)
 
 from invenio_override.generators import CommunityCreator
 
@@ -31,4 +43,60 @@ class CustomCommunitiesPermissionPolicy(CommunityPermissionPolicy):
     ]
 
     can_include_directly = [Administration(), SystemProcess()]
+
+
+class CurationRDMRequestsPermissionPolicy(RDMRequestsPermissionPolicy):
+    """Customized permission policy for sane handling of curation requests."""
+
+    curation_request_record_review = IfRequestTypes(
+        [CurationRequest],
+        then_=[TopicPermission(permission_name="can_review")],
+        else_=[],
+    )
+
+    # Only allow community-submission requests to be accepted after the rdm-curation
+    # request has been accepted
+    can_action_accept = [
+        IfRequestTypes(
+            request_types=[CommunitySubmission],
+            then_=[
+                IfCurationRequestAccepted(
+                    then_=RDMRequestsPermissionPolicy.can_action_accept, else_=[]
+                )
+            ],
+            else_=RDMRequestsPermissionPolicy.can_action_accept,
+        )
+    ]
+
+    # Update can read and can comment with new states
+    can_read = [
+        # Have to explicitly check the request type and circumvent using status,
+        # as creator/receiver will add a query filter where one entity must be the user.
+        IfRequestTypes(
+            [CurationRequest],
+            then_=[
+                Creator(),
+                Receiver(),
+                TopicPermission(permission_name="can_review"),
+            ],
+            else_=RDMRequestsPermissionPolicy.can_read,
+        )
+    ]
+
+    can_create_comment = can_read
+
+    # Update submit to also allow record reviewers/managers for curation requests
+    can_action_submit = RDMRequestsPermissionPolicy.can_action_submit + [
+        curation_request_record_review
+    ]
+    # Add new actions
+    can_action_review = RDMRequestsPermissionPolicy.can_action_accept
+    can_action_critique = RDMRequestsPermissionPolicy.can_action_accept
+
+    can_action_resubmit = can_action_submit
+
+
+class CurationRDMRecordPermissionPolicy(BaseCurationRDMRecordPermissionPolicy):
+    """Customized RDM record permission policy for curation workflow."""
+    pass
 
