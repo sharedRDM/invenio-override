3496cf08d2c7985d0ee7e3c817512b0ea60443cf
Eduard Nitu <edi.nitu2000@yahoo.com>
2026-08-11 11:11:19 +0200

    fix: small refactor for login icons

--- invenio_override/config.py
+++ invenio_override/config.py
@@ -170,8 +170,11 @@ OVERRIDE_SAML_INSTITUTION = "TUGRAZ"
 # ============================================================================
 # Branding and UI Customization
 # ============================================================================
-OVERRIDE_ICON = "images/icon_use.png"
-"""Icon used in login page"""
+OVERRIDE_OAUTH_ICON = "images/icon_use.png"
+"""Icon used in login page for oauth apps."""
+
+OVERRIDE_SAML_ICON = "images/icon_use.png"
+"""Icon used in login page for SAML IDPs."""
 
 OVERRIDE_EDUGAIN_ICON = "icons/eduGAIN-FavIcon-transparent.png"
 """Icon used for EDUGAIN login option."""
